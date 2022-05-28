e9dfd9eb1ebd22a0b5f31406b6d4bcaec5f1f77c
DESKTOP-O8UP5EQ\MYPC <raizen.sangalang@hoolisoftware.com>
2022-05-28 17:14:34 +0800

    sdkskds

--- cars copy.sql
+++ cars copy.sql
@@ -1,73 +1,43 @@
--- colors of the car 
 
-
--- create table colors of the car mysql
--- id not null auto_increment primary key
--- color varchar(255)
--- paint varchar(255)
 create table colors(
     id int not null auto_increment primary key,
     color varchar(255),
     paint varchar(255)
 );
-/*
-color Paint
-Red	Metallic
-Green	Gloss
-Blue	Metallic
-White	Gloss
-Black	Gloss
-*/
+
 insert into colors(color, paint) values('Red', 'Metallic');
 insert into colors(color, paint) values('Green', 'Gloss');
 insert into colors(color, paint) values('Blue', 'Metallic');
 insert into colors(color, paint) values('White', 'Gloss');
 insert into colors(color, paint) values('Black', 'Gloss');
 
--- create table brands 
--- id not null auto_increment primary key 
--- brand varchar(255)
+select * from colors;
+
 create table brands(
     id int not null auto_increment primary key,
     brand varchar(255)
 );
-/*
-Ford
-Toyota
-Nissan
-Smart
-BMW
-*/
+
 insert into brands(brand) values('Ford');
 insert into brands(brand) values('Toyota');
 insert into brands(brand) values('Nissan');
 insert into brands(brand) values('Smart');
 insert into brands(brand) values('BMW');
 
--- create table models
--- id not null auto_increment primary key
--- model varchar(255)
+select * from brands;
+
 create table models(
     id int not null auto_increment primary key,
     model varchar(255)
 );
-/*
-Sports
-Sedan 
-4WD
-Luxury
-*/
+
 insert into models(model) values('Sports');
 insert into models(model) values('Sedan');
 insert into models(model) values('4WD');
 insert into models(model) values('Luxury');
 
--- create cars table
--- from colors, brands, models
--- id not null auto_increment primary key
--- colors foreign key
--- brands foreign key
--- models foreign key
+select * from models;
+
 
 create table cars(
     id int not null auto_increment primary key,
@@ -75,21 +45,10 @@ create table cars(
     brand int not null,
     model int not null,    
     foreign key(color) references colors(id),
-    foreign key(brands) references brands(id),
-    foreign key(models) references models(id)
+    foreign key(brand) references brands(id),
+    foreign key(model) references models(id)
 );
-/*
-1	2	1
-3	1	2
-5	3	1
-4	4	2
-2	2	3
-3	5	4
-4	1	3
-2	2	1
-5	2	3
-4	5	1
-*/
+
 insert into cars(color, brand, model) values(1, 2, 1);
 insert into cars(color, brand, model) values(3, 1, 2);
 insert into cars(color, brand, model) values(5, 3, 1);
@@ -100,15 +59,22 @@ insert into cars(color, brand, model) values(4, 1, 3);
 insert into cars(color, brand, model) values(2, 2, 1);
 insert into cars(color, brand, model) values(5, 2, 3);
 insert into cars(color, brand, model) values(4, 5, 1);
--- select * from cars; 
--- make visible the foreign key data inner join
--- for example:
-/**
-id color model
-1	Red	Sports
-2	Red	Sedan
-**/
+
+select * from cars;
+-- output:
+-- +----+---------+---------+---------+
+-- | id | color   | brand   | model   |
+-- +----+---------+---------+---------+
+-- | 1  |        1|        2|        1|
+-- | 2  |        2|        2|        3|
+-- | 3  |        3|        1|        2|
+-- | 4  |        4|        5|        1|
+-- | 5  |        5|        2|        3|
+-- +----+---------+---------+---------+
+
 select cars.id, colors.color, models.model from cars inner join colors on cars.color = colors.id inner join models on cars.model = models.id order by cars.id;
 
+-- select from cars the model and colors where models is sports
+select cars.id as id ,models.model as model , colors.color as color from cars inner join colors on cars.color = colors.id inner join models on cars.model = models.id where models.model = 'Sports'
 
 
